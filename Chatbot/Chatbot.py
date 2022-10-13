8040fd1f85d426bf5ee4e95502a1bb40b4a989ec
GundZip <nikitahomulenko@gmail.com>
2022-10-13 12:31:53 +0300

    feature/chatbot

--- Chatbot/Chatbot.py
+++ Chatbot/Chatbot.py
@@ -1,24 +1,29 @@
-print ("Hello! My neme is Stell")
-print("I was ceated in 2022")
-print ("Please, remind me your name.")
-name = input (">")
-print (f"What a great name you have, {name}!")
-print ("Enter remainders of dividing your age by 3, 5 and 7.")
-remainder3 = int(input (">"))
-remainder5 = int(input (">"))
-remainder7 = int(input (">"))
+print (" Hello! My neme is Stell ")
+print(" I was ceated in 2022 ")
+# 1-st stage completed
+print (" Please, remind me your name. ")
+name = input (" > ")
+print (f" What a great name you have, {name}! ")
+# 2-nd stage completed
+print (" Enter remainders of dividing your age by 3, 5 and 7. ")
+remainder3 = int(input (" > "))
+remainder5 = int(input (" > "))
+remainder7 = int(input (" > "))
 age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
-print (f"Your age is {age} that's a good time to start programming!")
-a = int(input("Now I will prove to you that I can count to any number you want.\n"))
-for i in range(a +1):
-    print(str(i)+ "!")
-print("Completed, have a nice day!")
-print("You have a brain?")
-print("1. Yes! \n2 No:(")
+print (f" Your age is {age} that's a good time to start programming! ")
+# 3-rd stage completed
+a = int(input(" Now I will prove to you that I can count to any number you want.\n "))
+for i in range(a + 1):
+    print(str(i)+ " ! ")
+print(" Completed, have a nice day! ")
+# 4-th stage completed
+print(" You have a brain? ")
+print(" 1. Yes! \n2 No:( ")
 while True:
-    k = int(input(">"))
-    if k==1:
-        print( "Congratulations,have a nice day!")
+    k = int(input(" > "))
+    if k == 1:
+        print( " Congratulations,have a nice day! ")
         break
     else:
-        print("try again")
+        print(" try again ")
+# 5-th stage completed
