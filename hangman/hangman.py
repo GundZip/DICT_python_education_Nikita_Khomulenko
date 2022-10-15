bba55a23c71386dfe179a8f0b60154a8116460ac
GundZip <nikitahomulenko@gmail.com>
2022-10-15 14:44:27 +0300

    hangmen 6 stage

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -15,13 +15,17 @@ while life > 0:
         remember_letters.append(letter)
         all_letters.remove(letter)
     else:
-        life-=1
+        life-= 1
+        print("That letter doesn`s appear un the word")
+    if letter in remember_letters:
+        life -= 1
+        print("No improvements")
     words = ''.join([i if i in remember_letters else "_" for i in word])
     print(words)
     if len(all_letters) == 0:
         break
 print("thanks for playing\nWe'll see how well you did in the next stage")
-#5-th stage completed
+#6-th stage completed
 
 
 
