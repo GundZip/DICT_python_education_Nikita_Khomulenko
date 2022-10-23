9e6a41c6025cea083390a96cc87f097d8a825f2b
GundZip <nikitahomulenko@gmail.com>
2022-10-23 18:13:27 +0300

    dinnerparty 2-3rd stage

--- dinnerparty/dinnerparty.py
+++ dinnerparty/dinnerparty.py
@@ -9,3 +9,18 @@ print ('Enter the name of every friend (including you), each on a new line')
 for _ in range(amount):
     name = input(">")
     transactions[name] = 0
+price = int(input('Enter the totat amount:\n'))
+a = round(price/amount, 2)
+for name in transactions:
+    transactions[name] = a
+print(transactions)
+
+YoR = {'yes': True, 'no': False}
+
+print('Do you want to use "who is lucky?" feature\n type yes/no')
+lucky = YoR[input('>').lower()]
+l_name = '_'
+if lucky:
+    l_name = random.choice(list(transactions.keys()))
+    print(l_name, "is lucky today!")
+
