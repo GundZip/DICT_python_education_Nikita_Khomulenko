45e5e47a888e00ffe8f2f314865a406f4dc12c39
GundZip <nikitahomulenko@gmail.com>
2022-11-05 14:06:57 +0200

    stage 2

--- coffeemachine/coffeemachine.py
+++ coffeemachine/coffeemachine.py
@@ -1,8 +1,16 @@
-print("Starting to make a coffee")
-print("Grinding coffee beans")
-print("Boiling water")
-print("Mixing boiled water with crushed coffee beans")
-print("Pouring coffee into the cup")
-ptint("Pouring some milk into the cup")
-print("Coffee is ready!")
 
+
+#print("Starting to make a coffee")
+#print("Grinding coffee beans")
+#print("Boiling water")
+#print("Mixing boiled water with crushed coffee beans")
+#print("Pouring coffee into the cup")
+#ptint("Pouring some milk into the cup")
+#print("Coffee is ready!")
+
+print("Write how many cups of coffe you will need:")
+cups = int(input(">"))
+print("For", cups, "cups of coffe you will need:")
+print(cups * 50, "ml of milk")
+print (cups * 200, "ml of water")
+print(cups * 15, "ml of coffee beans")
