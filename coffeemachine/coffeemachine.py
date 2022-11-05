38aa35113fe663ab1b14f52b80b202b4ba453ccf
GundZip <nikitahomulenko@gmail.com>
2022-11-05 14:36:26 +0200

    stage 3

--- coffeemachine/coffeemachine.py
+++ coffeemachine/coffeemachine.py
@@ -8,9 +8,18 @@
 #ptint("Pouring some milk into the cup")
 #print("Coffee is ready!")
 
-print("Write how many cups of coffe you will need:")
-cups = int(input(">"))
-print("For", cups, "cups of coffe you will need:")
-print(cups * 50, "ml of milk")
-print (cups * 200, "ml of water")
-print(cups * 15, "ml of coffee beans")
+n_water = 180
+n_coffee = 15
+n_milk = 55
+
+water = int(input("Write how many ml of water the coffee machine has:\n>"))
+coffee = int(input("Write how many ml of milk the coffee machine has :\n"))
+milk = int(input("Write how many ml of milk the coffee machine has :\n"))
+need_cups = int(input("Write how many cups of coffee will tou need :\n "))
+cup_coffee = mun([water // n_water,milk // n_milk, coffee // n_coffee])
+if cup_coffee == need_cups:
+    print ("Yes, I can make that amount of coffee")
+elif cup_coffee > need_cups:
+    print("Yes, I can make that amount of coffee(and even", cup_coffee - need_cups, "more than that")
+else:
+    print("No, I can make only",cup_coffee,"cups of coffee")
