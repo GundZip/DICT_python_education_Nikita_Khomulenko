b65b345ad37d26a944f895c027a3b6fa9464f2ed
GundZip <nikitahomulenko@gmail.com>
2022-11-05 13:57:30 +0200

    stage 1

---
+++ coffeemachine/coffeemachine.py
@@ -0,0 +1,8 @@
+print("Starting to make a coffee")
+print("Grinding coffee beans")
+print("Boiling water")
+print("Mixing boiled water with crushed coffee beans")
+print("Pouring coffee into the cup")
+ptint("Pouring some milk into the cup")
+print("Coffee is ready!")
+
