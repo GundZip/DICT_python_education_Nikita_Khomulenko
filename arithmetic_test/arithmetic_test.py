93c811d4e36463e69531f26f3da188fdf9640c7a
GundZip <nikitahomulenko@gmail.com>
2023-03-04 14:21:44 +0200

    arithmetic_test_2stage

--- arithmetic_test/arithmetic_test.py
+++ arithmetic_test/arithmetic_test.py
@@ -1,5 +1,7 @@
 import random
 
+print("Hello user \n Power by Stell" )
+
 def generate_question():
     # генерація від 2 до 9
     num1 = random.randint(2, 9)
@@ -22,6 +24,31 @@ def main():
         print("Right!")
     else:
         print("Wrong!")
+def check_input(user_input):
+    try:
+        int(user_input)
+        return True
+    except ValueError:
+        print("Incorrect format.")
+        return False
 
+def main():
+    num_correct = 0
+    for i in range(5):
+        question, answer = generate_question()
+        print(question)
+        while True:
+            user_answer = input()
+            if check_input(user_answer):
+                break
+        if int(user_answer) == answer:
+            print("Right!")
+            num_correct += 1
+        else:
+            print("Wrong!")
+    #  кількість правильних відповідей
+    print(f"Your mark is {num_correct}/5.")
+    print("Goodbye \n Have a nice day")
 if __name__ == '__main__':
     main()
+
