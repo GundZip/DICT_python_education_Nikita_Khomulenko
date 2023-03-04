9e8877fa789ff364ec1637741778cfb8b946f905
GundZip <nikitahomulenko@gmail.com>
2023-03-04 15:05:02 +0200

    arithmetic_test_Final_stage

--- arithmetic_test/arithmetic_test.py
+++ arithmetic_test/arithmetic_test.py
@@ -1,54 +1,67 @@
 import random
 
-print("Hello user \n Power by Stell" )
+print("Hello user "
+      "\n Power by Stell" )
+name = input("Enter your name: ")
+def simple_operation(num1, num2, operation):
+    if operation == "+":
+        return num1 + num2
+    elif operation == "-":
+        return num1 - num2
+    elif operation == "*":
+        return num1 * num2
+    elif operation == "/":
+        return num1 // num2
 
-def generate_question():
-    # генерація від 2 до 9
-    num1 = random.randint(2, 9)
-    num2 = random.randint(2, 9)
-    operator = random.choice(["+", "-", "*"])
-    question = f"{num1} {operator} {num2} = "
-    if operator == "+":
-        answer = num1 + num2
-    elif operator == "-":
-        answer = num1 - num2
+def square(num):
+    return num * 2
+
+def test():
+    level = input("Choose the level of difficulty (1 or 2): ")
+    while level not in ["1", "2"]:
+        level = input("Incorrect format. Choose the level of difficulty (1 or 2): ")
+    if level == "1":
+        print("You chose level 1: simple operations with numbers 2-9.")
+        operations = ["+", "-", "*", "/"]
+        correct_answers = 0
+        for i in range(5):
+            num1 = random.randint(2, 9)
+            num2 = random.randint(2, 9)
+            operation = random.choice(operations)
+            answer = input(f"{i+1}. {num1} {operation} {num2} = ")
+            while not answer.isdigit():
+                answer = input("Incorrect format. Enter again: ")
+            if int(answer) == simple_operation(num1, num2, operation):
+                print("Right!")
+                correct_answers += 1
+            else:
+                print("Wrong!")
+        print(f"Your mark is {correct_answers}/5.")
+    elif level == "2":
+        print("You chose level 2: squaring numbers from 11 to 29.")
+        correct_answers = 0
+        for i in range(5):
+            num = random.randint(11, 29)
+            answer = input(f"{i+1}. {num}^2 = ")
+            while not answer.isdigit():
+                answer = input("Incorrect format. Enter again: ")
+            if int(answer) == square(num):
+                print("Right!")
+                correct_answers += 1
+            else:
+                print("Wrong!")
+        print(f"Your mark is {correct_answers}/5.")
+    save_result = input("Would you like to save your result to the file? Enter yes or no: ")
+    if save_result.lower() == "yes" or save_result.lower() == "y""YES":
+        name = input("Enter your name: ")
+        print("you progress save a results.txt "
+              "\n Process completed"
+              " \n Have a nice day")
+        level_description = "simple operations with numbers 2-9" if level == "1" else "squaring numbers from 11 to 29"
+        with open("results.txt", "a") as f:
+            f.write(f"{name}: {correct_answers}/5 in level {level} ({level_description}).\n")
     else:
-        answer = num1 * num2
-    return question, answer
+        return
 
-def main():
-    question, answer = generate_question()
-    print(question)
-    user_answer = int(input())
-    if user_answer == answer:
-        print("Right!")
-    else:
-        print("Wrong!")
-def check_input(user_input):
-    try:
-        int(user_input)
-        return True
-    except ValueError:
-        print("Incorrect format.")
-        return False
-
-def main():
-    num_correct = 0
-    for i in range(5):
-        question, answer = generate_question()
-        print(question)
-        while True:
-            user_answer = input()
-            if check_input(user_answer):
-                break
-        if int(user_answer) == answer:
-            print("Right!")
-            num_correct += 1
-        else:
-            print("Wrong!")
-    #  кількість правильних відповідей
-    print(f"Your mark is {num_correct}/5.")
-    print("Goodbye \n Have a nice day")
-if __name__ == '__main__':
-    main()
+test()
 
