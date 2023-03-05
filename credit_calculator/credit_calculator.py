42eaf9204ba001324c969c285747e41c73f207b3
GundZip <nikitahomulenko@gmail.com>
2023-03-05 16:43:27 +0200

    feature/credit_calculator_stage_4.0

--- credit_calculator/credit_calculator.py
+++ credit_calculator/credit_calculator.py
@@ -1,10 +1,12 @@
 import math
+
 print('Power by Stell \n'
       'Hello \n '
       "What do you want to calculate?")
 print("type 'n' for number of monthly payments, \n"
       "type 'a' for annuity monthly payment amount,\n"
-      "type 'p' for annuity monthly payment amount,")
+      "type 'p' for annuity monthly payment amount,\n"
+      "type 'd' for differentiated payment")
 
 calculation_type = input()
 
@@ -17,14 +19,16 @@ if calculation_type == 'n':
     interest = float(input()) / 1200
 
     n = math.ceil(math.log(monthly_payment / (monthly_payment - interest * loan_principal), 1 + interest))
-    years, months = divmod(n, 12)
-
-    if years == 0:
-        print("It will take {} months to repay this loan!".format(months))
-    elif months == 0:
-        print("It will take {} years to repay this loan!")
+    if n > 0:
+        years, months = divmod(n, 12)
+        if years == 0:
+            print("It will take {} months to repay this loan!".format(months))
+        elif months == 0:
+            print("It will take {} years to repay this loan!".format(years))
+        else:
+            print("It will take {} years and {} months to repay this loan!".format(years, months))
     else:
-        print("It will take {} years and {} months to repay this loan!".format(years, months))
+        print("The loan cannot be repaid with this monthly payment!")
 
 elif calculation_type == 'a':
     print("Enter the loan principal:")
@@ -62,6 +66,24 @@ elif calculation_type == 'p':
     else:
         print("Your loan principal = {} with {} years and {} months to repay"
               .format(round(loan_principal), years, months))
+
+elif calculation_type == 'd':
+    print("Enter the loan principal:")
+    loan_principal = float(input())
+    print("Enter the number of periods:")
+    n = int(input())
+    print("Enter the loan interest:")
+    interest = float(input()) / 1200
+
+    total_payment = 0
+
+    for month in range(1,n +1):
+        differentiated_payment = loan_principal / n + interest * (loan_principal - loan_principal * (month - 1) / n)
+        total_payment += differentiated_payment
+        print("Month {}: payment is {}".format(month, math.ceil(differentiated_payment)))
+
+        overpayment = total_payment - loan_principal
+        print("\nOverpayment = {}".format(round(overpayment)))
 
 else:
     print("Invalid calculation type.")
