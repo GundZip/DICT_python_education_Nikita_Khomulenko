86e0203657d8da70c63293408e4a2cf8aed09221
GundZip <nikitahomulenko@gmail.com>
2023-03-05 15:45:15 +0200

    feature/credit_calculator_stage_2.3

--- credit_calculator/credit_calculator.py
+++ credit_calculator/credit_calculator.py
@@ -1,17 +1,67 @@
+import math
 print('Power by Stell \n'
-      'Hello')
-principal = float(input("Enter the loan principal: "))
-interest_rate = float(input("Enter the interest rate: "))
+      'Hello \n '
+      "What do you want to calculate?")
+print("type 'n' for number of monthly payments, \n"
+      "type 'a' for annuity monthly payment amount,\n"
+      "type 'p' for annuity monthly payment amount,")
 
-months = int(input("Enter the number of months: "))
+calculation_type = input()
 
-monthly_payment = principal * (interest_rate * (1 + interest_rate) ** months) / ((1 + interest_rate) ** months - 1)
-total_payment = monthly_payment * months
+if calculation_type == 'n':
+    print("Enter the loan principal:")
+    loan_principal = float(input())
+    print("Enter the monthly payment:")
+    monthly_payment = float(input())
+    print("Enter the loan interest:")
+    interest = float(input()) / 1200
 
-for i in range(1, months + 1):
-    interest = principal * interest_rate
-    principal -= monthly_payment - interest
-    print(f"Month {i}: repaid {round(monthly_payment, 2)}")
+    n = math.ceil(math.log(monthly_payment / (monthly_payment - interest * loan_principal), 1 + interest))
+    years, months = divmod(n, 12)
 
-print(f"\nThe loan has been repaid!\nTotal payment: {round(total_payment, 2)}")
-print ('Have a nice day')
+    if years == 0:
+        print("It will take {} months to repay this loan!".format(months))
+    elif months == 0:
+        print("It will take {} years to repay this loan!")
+    else:
+        print("It will take {} years and {} months to repay this loan!".format(years, months))
+
+elif calculation_type == 'a':
+    print("Enter the loan principal:")
+    loan_principal = float(input())
+    print("Enter the number of periods:")
+    n = int(input())
+    print("Enter the loan interest:")
+    interest = float(input()) / 1200
+
+    annuity_payment = loan_principal * (interest * (1 + interest)**n) / ((1 + interest)**n - 1)
+
+    years, months = divmod(n, 12)
+    if years == 0:
+        print("You need to pay {} per month for {} months".format(round(annuity_payment), months))
+    elif months == 0:
+        print("You need to pay {} per month for {} years".format(round(annuity_payment), years))
+    else:
+        print("You need to pay {} per month for {} years and {} months".format(round(annuity_payment), years, months))
+
+elif calculation_type == 'p':
+    print("Enter the annuity payment:")
+    annuity_payment = float(input())
+    print("Enter the number of periods:")
+    n = int(input())
+    print("Enter the loan interest:")
+    interest = float(input()) / 1200
+
+    loan_principal = annuity_payment / ((interest * (1 + interest)**n) / ((1 + interest)**n - 1))
+
+    years, months = divmod(n, 12)
+    if years == 0:
+        print("Your loan principal = {} with {} months to repay".format(round(loan_principal), months))
+    elif months == 0:
+        print("Your loan principal = {} with {} years to repay".format(round(loan_principal), years))
+    else:
+        print("Your loan principal = {} with {} years and {} months to repay"
+              .format(round(loan_principal), years, months))
+
+else:
+    print("Invalid calculation type.")
