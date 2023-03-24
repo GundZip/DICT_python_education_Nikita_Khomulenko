272aee8fdba32dca0ee19b00cd85db17d9d079a3
GundZip <nikitahomulenko@gmail.com>
2023-03-24 20:05:58 +0200

    1-st stage

---
+++ currency_exchange/currency_exchange.py
@@ -0,0 +1,7 @@
+print("Product by Stell \n"
+      "Hello")
+money = float(input("Please, enter the number of mycoins you have:"))
+rate = float(input("Please, enter the exchange rate:"))
+grn = money*rate
+print("The total amount of dollars:",round(grn,2))
+print("Have a nice day")
