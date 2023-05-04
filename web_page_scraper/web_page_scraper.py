6db8a7c99e64575004308c95673e23cf509dd9ec
GundZip <nikitahomulenko@gmail.com>
2023-05-04 21:16:50 +0300

    Stage-2/3

--- web_page_scraper/web_page_scraper.py
+++ web_page_scraper/web_page_scraper.py
@@ -1,12 +1,25 @@
 import requests
-import json
+from bs4 import BeautifulSoup
+import string
 
-url = input("Enter the URL: ")
+url = "https://www.nature.com/nature/articles?sort=PubDate&year=2022&page=3"
+r = requests.get(url)
 
-response = requests.get(url)
+soup = BeautifulSoup(r.content, "html.parser")
 
-if response.status_code != 200 or '"' not in response.text:
-    print("Invalid quote resource!")
-else:
-    quote = json.loads(response.text)["content"]
-    print("Quote: ", quote)
+articles = soup.find_all("article")
+
+for article in articles:
+    article_type = article.find("span", {"data-test": "article.type"}).text.strip()
+
+    if article_type == "News":
+        article_link = article.find("a", {"data-track-action": "view article"})
+        article_title = article_link.text.strip() if article_link else "untitled"
+        article_title = article_title.translate(str.maketrans('', '', string.punctuation)).replace(" ", "_")
+        article_body = article.find("div", {"class": "c-article-body"})
+        article_text = article_body.text.strip() if article_body else "no content"
+        article_text = article_text.replace("\n", "").replace("\r", "").replace("\t", "").replace("\xa0", " ")
+        article_title = article_title.replace(" ", "_")
+        with open(f"{article_title}.txt", "w", encoding="utf-8") as file:
+            file.write(article_text)
+        print(f"{article_title}.txt saved successfully.")
